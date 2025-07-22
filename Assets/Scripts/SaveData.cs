ee81422abb895fd30b404910f0037c6a7d54092b
LorenaPerezHernando <lorenaperezhernando@gmail.com>
2025-07-22 20:37:51 +0200

    Save System, Save Data 1

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SaveData : MonoBehaviour
+{
+    public Transform playerPos;
+    public int puzzlesDone;
+    public int guardianes;
+    public int level; 
+
+    //Public int objetos del inventario
+}
--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using System.IO;
 
 public class SaveSystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private static string savePath = Application.persistentDataPath + "/save.json";
+
+    public static void Save(SaveData data)
     {
-        
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(savePath, json);
+        Debug.Log("Game Saved to: " + savePath);
     }
 
-    // Update is called once per frame
-    void Update()
+    public static SaveData Load()
     {
-        
+        if (File.Exists(savePath))
+        {
+            string json = File.ReadAllText(savePath);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            return data;
+        }
+        else
+        {
+            Debug.LogWarning("Save file not found, returning new data.");
+            return new SaveData();
+        }
+    }
+
+    public static void Delete()
+    {
+        if (File.Exists(savePath))
+        {
+            File.Delete(savePath);
+            Debug.Log("Save file deleted.");
+        }
     }
 }
