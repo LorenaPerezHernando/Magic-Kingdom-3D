41c4b544e191592618321194bd9941d432157913
LorenaPerezHernando <lorenaperezhernando@gmail.com>
2025-07-29 01:33:59 +0200

    Details for Plant Puzzle

--- Assets/Scripts/Puzzles/PuzzleWaterPlants.cs
+++ Assets/Scripts/Puzzles/PuzzleWaterPlants.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class PuzzleWaterPlants : MonoBehaviour
 {
+    [Header("Finished Puzzle")]
+    [SerializeField] private Animator _farmerAnim;
+    [SerializeField] private GameObject _rainVFX;
+
+    [Header("Puzzle")]
     [SerializeField] private int _plantsWatered;
     private void OnTriggerEnter(Collider other)
     {
@@ -12,8 +17,13 @@ public class PuzzleWaterPlants : MonoBehaviour
             if(_plantsWatered >= 3)
             {
                 print("Rewards");
-                //TODO Rewards : Women talks (me has ayudado a regar, muchas gracias, no se que me ha pasado,
+                //TODO Rewards : 
+                _farmerAnim.SetBool("Idle", true);
+                _rainVFX.SetActive(false);
+                //TODO farmer looks at player
+                //TODO farmer talks : Women talks (me has ayudado a regar, muchas gracias, no se que me ha pasado,
                 //Puedes quedarte lo que quieras de mi jardin
+
             }
         }
     }
